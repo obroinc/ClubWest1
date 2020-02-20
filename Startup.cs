2992b8485c4355a5e41741e66250a996520bb8b6
HPLaptop <HPLaptop@CLEMENTLAPTOP>
2020-02-20 20:24:49 +0000

    test branch commit

--- Startup.cs
+++ Startup.cs
@@ -37,6 +37,8 @@ namespace ClubWest
             services.AddRazorPages().AddRazorRuntimeCompilation();
         }
 
+        //more code here
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
